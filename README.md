5b2bd3ed92e318e9e98dac874dcd80362166a272
Patrick Robinson <patrick.robinson@bertelsmann.de>
2022-06-21 10:18:49 +0200

    Init
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# terraform-opswatch-metric-stream
+# terraform-module-vpc
+This module creates a workload VPC. It can be configured with either its own NAT or as a spoke VPC in the central TGW design. This requires both an existing deployment of the tgw-central module once, and a call to vpc-tgw-attachment for each of these VPCs.
+
+The VPCs CIDR is a variable. If you want to combine it with FNT ordered CIDRs, this module won't do that, but you can provide so by the variable. Per default, unique CIDRs aren't assumed.
+# TF Module Documentation
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 3.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | >= 3.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_cloudwatch_metric_stream.opswatch](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_metric_stream) | resource |
+| [aws_iam_role.cloudwatch](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role.kinesis](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy.firehose_delivery](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_iam_role_policy.http_delivery](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_kinesis_firehose_delivery_stream.opswatch](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kinesis_firehose_delivery_stream) | resource |
+| [aws_s3_bucket.opswatch_error](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket) | resource |
+| [aws_s3_bucket_acl.opswatch_error](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl) | resource |
+| [aws_s3_bucket_lifecycle_configuration.opswatch_error](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration) | resource |
+| [aws_s3_bucket_public_access_block.opswatch_error](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block) | resource |
+| [aws_s3_bucket_server_side_encryption_configuration.opswatch_error](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_server_side_encryption_configuration) | resource |
+| [aws_s3_bucket_versioning.opswatch_error](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_url"></a> [url](#input\_url) | Opswatch URL | `string` | n/a | yes |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->    
---
+++ metrics-stream.tf
@@ -0,0 +1,145 @@
+resource "aws_cloudwatch_metric_stream" "opswatch" {
+  depends_on = [aws_iam_role_policy.firehose_delivery]
+  name = "OpswatchMetricStream"
+  output_format = "json"
+  firehose_arn = aws_kinesis_firehose_delivery_stream.opswatch.arn
+  role_arn = aws_iam_role.cloudwatch.arn
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "streams.metrics.cloudwatch.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "firehose_delivery" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "firehose:PutRecord",
+        "firehose:PutRecordBatch"
+      ]
+      Resource = aws_kinesis_firehose_delivery_stream.opswatch.arn
+    }]
+  })
+  role   = aws_iam_role.cloudwatch.id
+}
+
+resource "aws_iam_role" "kinesis" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "firehose.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "http_delivery" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"
+      ]
+      Resource = [
+        aws_s3_bucket.opswatch_error.arn,
+        "${aws_s3_bucket.opswatch_error.arn}/*"
+      ]
+    }]
+  })
+  role = aws_iam_role.kinesis.id
+}
+
+variable "url" {
+  type = string
+  description = "Opswatch URL"
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "opswatch" {
+  depends_on = [aws_iam_role_policy.http_delivery]
+  name = "OpswatchMetricStream"
+  destination = "http_endpoint"
+  http_endpoint_configuration {
+    url = var.url
+    name = "CentralMetricProcessor"
+    role_arn = aws_iam_role.kinesis.arn
+    buffering_size = 1
+    buffering_interval = 60
+    retry_duration = 100
+    s3_backup_mode = "FailedDataOnly"
+  }
+  s3_configuration {
+    role_arn           = aws_iam_role.kinesis.arn
+    bucket_arn         = aws_s3_bucket.opswatch_error.arn
+    buffer_size        = 128
+    buffer_interval    = 900
+    compression_format = "GZIP"
+  }
+}
+
+resource "aws_s3_bucket" "opswatch_error" {}
+
+resource "aws_s3_bucket_acl" "opswatch_error" {
+  bucket = aws_s3_bucket.opswatch_error.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "opswatch_error" {
+  bucket = aws_s3_bucket.opswatch_error.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "opswatch_error" {
+  bucket = aws_s3_bucket.opswatch_error.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "opswatch_error" {
+  bucket = aws_s3_bucket.opswatch_error.id
+
+  rule {
+    id = "expire"
+
+    expiration {
+      days = 1
+    }
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 1
+    }
+
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "opswatch_error" {
+  bucket = aws_s3_bucket.opswatch_error.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
