2de4c3c66b797008dfe612d941dd3d4b317b83a7
Patrick Robinson <patrick.robinson@bertelsmann.de>
2025-02-10 15:59:38 +0100

    Lint
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- metrics-stream.tf
+++ metrics-stream.tf
@@ -1,19 +1,19 @@
 variable "included_namespaces" {
-  type = list(string)
+  type    = list(string)
   default = []
 }
 
 variable "excluded_namespaces" {
-  type = list(string)
+  type    = list(string)
   default = []
 }
 
 resource "aws_cloudwatch_metric_stream" "opswatch" {
-  depends_on = [aws_iam_role_policy.firehose_delivery]
-  name = "OpswatchMetricStream"
+  depends_on    = [aws_iam_role_policy.firehose_delivery]
+  name          = "OpswatchMetricStream"
   output_format = "json"
-  firehose_arn = aws_kinesis_firehose_delivery_stream.opswatch.arn
-  role_arn = aws_iam_role.cloudwatch.arn
+  firehose_arn  = aws_kinesis_firehose_delivery_stream.opswatch.arn
+  role_arn      = aws_iam_role.cloudwatch.arn
 
   dynamic "include_filter" {
     for_each = var.included_namespaces
@@ -54,7 +54,7 @@ resource "aws_iam_role_policy" "firehose_delivery" {
       Resource = aws_kinesis_firehose_delivery_stream.opswatch.arn
     }]
   })
-  role   = aws_iam_role.cloudwatch.id
+  role = aws_iam_role.cloudwatch.id
 }
 
 resource "aws_iam_role" "kinesis" {
@@ -93,22 +93,22 @@ resource "aws_iam_role_policy" "http_delivery" {
 }
 
 variable "url" {
-  type = string
+  type        = string
   description = "Opswatch URL"
 }
 
 resource "aws_kinesis_firehose_delivery_stream" "opswatch" {
-  depends_on = [aws_iam_role_policy.http_delivery]
-  name = "OpswatchMetricStream"
+  depends_on  = [aws_iam_role_policy.http_delivery]
+  name        = "OpswatchMetricStream"
   destination = "http_endpoint"
   http_endpoint_configuration {
-    url = "${var.url}/metrics"
-    name = "CentralMetricProcessor"
-    role_arn = aws_iam_role.kinesis.arn
-    buffering_size = 1
+    url                = "${var.url}/metrics"
+    name               = "CentralMetricProcessor"
+    role_arn           = aws_iam_role.kinesis.arn
+    buffering_size     = 1
     buffering_interval = 60
-    retry_duration = 100
-    s3_backup_mode = "FailedDataOnly"
+    retry_duration     = 100
+    s3_backup_mode     = "FailedDataOnly"
     request_configuration {
       content_encoding = "GZIP"
     }
@@ -163,9 +163,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "opswatch_error" {
 }
 
 resource "aws_s3_bucket_public_access_block" "opswatch_error" {
-  bucket = aws_s3_bucket.opswatch_error.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.opswatch_error.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
